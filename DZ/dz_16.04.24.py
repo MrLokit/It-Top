193df6afb581f0eb5fb13909d0e85898f1af5d09
MrLokit <erosenkot7@gmail.com>
2024-04-14 16:52:09 +0300

    add dz  16-23.04.24

--- DZ/dz_16.04.24.py
+++ DZ/dz_16.04.24.py
@@ -7,6 +7,6 @@ print(objs)
 for obj in objs:
     p = os.path.join(dir_name, obj)
     if os.path.isfile(p):
-        print(f'{obj} - file {os.path.getsize(p)} bytes')  # исправленная команда
+        print(f'{obj} - file {os.path.getsize(p)} bytes')
     elif os.path.isdir(p):
         print(f'{obj} - dir')
